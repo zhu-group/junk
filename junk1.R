cb19a2aa3f22572d76022e3a74f3b9e38f5cb083
LEI ZHU <zhul3@sustech.edu.cn>
2020-09-24 19:47:41 +0800

    I add a new R file into the folder. This R script is used to have fun...

---
+++ junk1.R
@@ -0,0 +1,19 @@
+# Define the function FindTop()
+FindTop      <- function(N){
+  MoviePage  <- readLines('https://movie.douban.com/top250', encoding='UTF-8')
+  Pattarn    <- '<span class=\"title\">&nbsp;/&nbsp;([^<]*)</span>'
+  Lines      <- grep(Pattarn, MoviePage, value=TRUE)
+  Lines      <- sub(Pattarn,  '\\1', Lines )
+  Names      <- sub('^ *', '', Lines)
+  Names      <- sub('&#39;', "'", Names)
+  print(Names[1:N])
+}
+
+# Call the function with argument 5 and 10
+FindTop(5)
+FindTop(25)
+
+x <- seq(1,10,1)
+which(x==1 && x==2)
+
+x==1
